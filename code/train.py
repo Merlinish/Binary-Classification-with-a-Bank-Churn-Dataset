3fac6d5dba79bc23996c86965a975d7cb1e3d3ea
jerry <632973868@qq.com>
2024-01-10 22:10:08 +0800

    add train.py

---
+++ code/train.py
@@ -0,0 +1,91 @@
+import torch
+import os
+import numpy as np
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import random_split
+from datetime import datetime
+
+from train_loaddata import DataLoad
+
+model_path = '../model/{0:%Y-%m-%d}.pth'.format(datetime.now())
+
+
+def train(device, model, data_dir, epochs=200, batch_size=1, lr=0.00001):
+    data = DataLoad(data_dir)
+
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    net = model.to(device)
+
+    val_length = int(len(data) / 9)
+    train_length = len(data) - val_length
+    train_data, val_data = random_split(dataset=data, lengths=[train_length, val_length],
+                                        generator=torch.Generator())
+    # print("train: ", len(train_data))
+    # print("val: ", len(val_data))
+    train_data = torch.utils.data.DataLoader(dataset=train_data,
+                                             batch_size=batch_size,
+                                             shuffle=True)
+    val_data = torch.utils.data.DataLoader(dataset=val_data,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+    # print("train/batch: ", len(train_data))
+    # print("val/batch: ", len(val_data))
+
+    # weight_decay=1e-8, momentum=0.9
+    optimizer = optim.RMSprop(net.parameters(), lr=lr,
+                              weight_decay=1e-8, momentum=0.9)
+
+    #     criterion = nn.BCEWithLogitsLoss()
+    #     criterion = nn.SmoothL1Loss(reduction="sum")
+    criterion = nn.L1Loss()
+
+    for epoch in range(epochs):
+
+        print("epoch: {}".format(epoch))
+        net.train()
+
+        for image, label in train_data:
+            optimizer.zero_grad()
+
+            image = image.to(device=device, dtype=torch.float32)
+            label = label.to(device=device, dtype=torch.float32)
+
+            pred = net(image)
+            loss = criterion(pred, label)
+
+            if loss < best_loss:
+                best_loss = loss
+                torch.save(net.state_dict(), model_path)
+
+            loss.backward()
+            optimizer.step()
+        print('Loss/train', best_loss.item())
+
+        val_loss_line = []
+
+        net.eval()  # supposed to add this
+        with torch.no_grad():
+            for image, label in val_data:
+                image = image.to(device=device, dtype=torch.float32)
+                label = label.to(device=device, dtype=torch.float32)
+                output = net(image)
+                val_loss = criterion(output, label)
+                val_loss_line.append(val_loss.item())
+
+        if np.mean(val_loss_line) < best_val_loss:
+            best_val_loss = np.mean(val_loss_line)
+            torch.save(net.state_dict(), model_path)
+        print("loss/val", best_val_loss)
+
+    return 0
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_dir = '../data/train/train_shaped.csv'
+
+    train(device, model, data_dir)
